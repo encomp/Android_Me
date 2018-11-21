96bdbfdda0bf3eb527f04024571976f06560af8a
encomp <edgar.martinez.rico@gmail.com>
2018-11-21 13:25:13 -0800

    Solved exercise 4.

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public final class MasterListFragment extends Fragment {
+
+  private MasterListAdapter masterListAdapter;
+
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+    GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
+    masterListAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+    gridView.setAdapter(masterListAdapter);
+
+    return rootView;
+  }
+}
